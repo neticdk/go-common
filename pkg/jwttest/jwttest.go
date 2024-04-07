9ebcd9a48a4298c8375e1840029106a9b25c3f0f
Thor Anker Kvisgård Lange <tal@netic.dk>
2024-04-07 16:56:00 +0200

    feat: Changing the filtering a bit to allow for changing the default rego rules

--- pkg/jwttest/jwttest.go
+++ pkg/jwttest/jwttest.go
@@ -11,12 +11,13 @@ import (
 var SampleSecretKey = []byte("SharedSecret")
 
 // GenerateJwt is a test function to generate a JWT structure similar the one received from Keycloak
-func GenerateJwt(t *testing.T, roles map[string][]string) string {
+func GenerateJwt(t *testing.T, iss string, roles map[string][]string) (*jwt.Token, string) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 	claims["sub"] = "aae9782e-fee4-4423-bcbd-2252397683fb"
-	claims["iss"] = "https://keycloak.netic.dk/auth/realms/mcs"
+	claims["iss"] = iss
 	claims["exp"] = time.Now().Add(10 * time.Minute)
+	claims["azp"] = "inventory.k8s.netic.dk"
 	access := make(map[string]interface{})
 	for c, r := range roles {
 		access[c] = map[string]interface{}{
@@ -25,6 +26,7 @@ func GenerateJwt(t *testing.T, roles map[string][]string) string {
 	}
 	claims["resource_access"] = access
 	tokenString, err := token.SignedString(SampleSecretKey)
+	token.Raw = tokenString
 	require.NoError(t, err)
-	return tokenString
+	return token, tokenString
 }
