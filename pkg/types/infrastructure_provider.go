37f96e8dca9f9fb8ae8e8e245c8239d374b9ed64
Kim Nørgaard <kn@netic.dk>
2024-04-21 14:13:13 +0200

    feat: add infrastructure provider and resilience zone types

---
+++ pkg/types/infrastructure_provider.go
@@ -0,0 +1,40 @@
+package types
+
+type (
+	InfrastructureProvider  string
+	InfrastructureProviders []InfrastructureProvider
+)
+
+const (
+	NeticInfrastructureProvider InfrastructureProvider = "netic"
+	AzureInfrastructureProvider InfrastructureProvider = "azure"
+	AWSInfrastructureProvider   InfrastructureProvider = "aws"
+)
+
+func (p InfrastructureProvider) String() string {
+	return string(p)
+}
+
+func AllInfrastructureProviders() InfrastructureProviders {
+	return InfrastructureProviders{NeticInfrastructureProvider, AzureInfrastructureProvider, AWSInfrastructureProvider}
+}
+
+func AllInfrastructureProvidersString() (providers []string) {
+	for _, p := range AllInfrastructureProviders() {
+		providers = append(providers, p.String())
+	}
+	return
+}
+
+func ParseInfrastructureProvider(name string) (InfrastructureProvider, bool) {
+	switch name {
+	case "netic":
+		return NeticInfrastructureProvider, true
+	case "azure":
+		return AzureInfrastructureProvider, true
+	case "aws":
+		return AWSInfrastructureProvider, true
+	default:
+		return "", false
+	}
+}
