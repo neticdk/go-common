7e9cd2866a336c2cbf82c0e2cea8c04d81c3498b
Thor Anker Kvisgård Lange <tal@netic.dk>
2024-05-03 07:59:12 +0200

    feat: Adjusting logging a little bit

--- pkg/log/trace_handler.go
+++ pkg/log/trace_handler.go
@@ -8,23 +8,23 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
-type wrapper struct {
+type traceHandler struct {
 	handler slog.Handler
 }
 
 // NewJSONTraceIDHandler creates new [slog.JSONHandler] which adds a `TraceID` attribute to all loggings if trace id is present.
 func NewJSONTraceIDHandler(w io.Writer, opts *slog.HandlerOptions) slog.Handler {
-	h := &wrapper{
+	h := &traceHandler{
 		handler: slog.NewJSONHandler(w, opts),
 	}
 	return h
 }
 
-func (h *wrapper) Enabled(ctx context.Context, level slog.Level) bool {
+func (h *traceHandler) Enabled(ctx context.Context, level slog.Level) bool {
 	return h.handler.Enabled(ctx, level)
 }
 
-func (h *wrapper) Handle(ctx context.Context, record slog.Record) error {
+func (h *traceHandler) Handle(ctx context.Context, record slog.Record) error {
 	span := trace.SpanFromContext(ctx)
 	if span.SpanContext().HasTraceID() {
 		record = record.Clone()
@@ -33,14 +33,14 @@ func (h *wrapper) Handle(ctx context.Context, record slog.Record) error {
 	return h.handler.Handle(ctx, record)
 }
 
-func (h *wrapper) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return &wrapper{
+func (h *traceHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &traceHandler{
 		handler: h.handler.WithAttrs(attrs),
 	}
 }
 
-func (h *wrapper) WithGroup(name string) slog.Handler {
-	return &wrapper{
+func (h *traceHandler) WithGroup(name string) slog.Handler {
+	return &traceHandler{
 		handler: h.handler.WithGroup(name),
 	}
 }
