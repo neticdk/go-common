66b843dee19cbc540d35c871089a6a201f86251c
Kim Nørgaard <kn@netic.dk>
2025-02-20 09:54:18 +0100

    fix: feedback from ASH
    1. Link to packages in tut ["cmd", "errors", "ui"]
    2. link from first mention of ExecutionContext to https://github.com/neticdk/go-common/blob/main/pkg/cli/TUTORIAL.md#the-execution-context
    3. Explain var version = "HEAD"
    4. Limited explanation of what the different examples does in section: https://github.com/neticdk/go-common/blob/main/pkg/cli/TUTORIAL.md#using-the-executioncontext
    5. OutputFormat Table please
    6. perhaps make it more clear that <flag>Enabled does not enable it but "simply" makes the pflag available for use https://github.com/neticdk/go-common/blob/main/pkg/cli/TUTORIAL.md#persistentglobal-flags
    7. Example of good vs bad Run() i.e. "short vs long" "When should i move code out of Run() to usecases"

--- pkg/cli/cmd/output.go
+++ pkg/cli/cmd/output.go
@@ -1,15 +1,9 @@
 package cmd
 
-// OutputFormat represents the output format for the command
-type OutputFormat string
-
 const (
-	OutputFormatPlain    OutputFormat = "plain"
-	OutputFormatJSON     OutputFormat = "json"
-	OutputFormatYAML     OutputFormat = "yaml"
-	OutputFormatMarkdown OutputFormat = "markdown"
+	OutputFormatPlain    = "plain"
+	OutputFormatJSON     = "json"
+	OutputFormatYAML     = "yaml"
+	OutputFormatMarkdown = "markdown"
+	OutputFormatTable    = "table"
 )
-
-func (o OutputFormat) String() string {
-	return string(o)
-}
