91e15b422bd6520ded6bd274ca5597789006fd36
Alexander Soelberg Heidarsson <89837986+alex5517@users.noreply.github.com>
2025-03-18 07:39:35 +0100

    :recycle: Deprecate and move errors package to cmd package (#82)

--- pkg/cli/errors/general.go
+++ pkg/cli/errors/general.go
@@ -1,5 +1,11 @@
+// Deprecated: errors is deprecated and has been moved to github.com/neticdk/go-common/pkg/cli/cmd
+//
+// It has been deprecated due to the fact that it conflicts with the standard library errors package.
+// Please use directly from cmd package instead.
+// Expect the errors package to be removed in later versions.
 package errors
 
+// Deprecated: use github.com/neticdk/go-common/pkg/cli/cmd instead.
 type GeneralError struct {
 	Message string
 	HelpMsg string
@@ -7,7 +13,14 @@ type GeneralError struct {
 	Err     error
 }
 
+// Deprecated: use github.com/neticdk/go-common/pkg/cli/cmd instead.
 func (e *GeneralError) Error() string { return e.Message }
-func (e *GeneralError) Help() string  { return e.HelpMsg }
+
+// Deprecated: use github.com/neticdk/go-common/pkg/cli/cmd instead.
+func (e *GeneralError) Help() string { return e.HelpMsg }
+
+// Deprecated: use github.com/neticdk/go-common/pkg/cli/cmd instead.
 func (e *GeneralError) Unwrap() error { return e.Err }
-func (e *GeneralError) Code() int     { return e.CodeVal }
+
+// Deprecated: use github.com/neticdk/go-common/pkg/cli/cmd instead.
+func (e *GeneralError) Code() int { return e.CodeVal }
